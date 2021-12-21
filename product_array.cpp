675f28a0dd908ab076c353957fd431f3aced8925
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2021-12-21 20:15:54 +0530

    Update product_array.cpp

--- product_array.cpp
+++ product_array.cpp
@@ -1,4 +1,12 @@
-//template overloading in c++
+/*Given an array arr[] of n integers, construct a Product Array prod[] (of same size) such that prod[i] is equal to the product of all the elements of arr[] except arr[i]. Solve it without division operator in O(n) time.
+
+Example : 
+
+Try Amazon Test Series that Includes topic-wise practice questions on all important DSA topics along with 10 practice contests of 2 hours each. Designed with years of experience.
+
+Input: arr[]  = {10, 3, 5, 6, 2}
+
+Output: prod[]  = {180, 600, 360, 300, 900}*/
 #include<iostream>
 using namespace std;
 int main()
